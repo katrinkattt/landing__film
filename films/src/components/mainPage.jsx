0d85a058dec041c0ddb1f8ff16c100cb46b692bf
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-26 16:09:30 +0500

    add Menu left in this app

--- films/src/components/mainPage.jsx
+++ films/src/components/mainPage.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import MenuLeft from './menuLeft'
 
 export default class MainPage extends Component{
     render(){
         return(
             <div className="main__page">
-                
+                <MenuLeft />
             </div>
         )
     }
