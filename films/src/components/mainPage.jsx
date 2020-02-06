7f455463be53d83013a076119efa9368f0664451
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-02-06 23:41:48 +0500

    Update mainPage.jsx

--- films/src/components/mainPage.jsx
+++ films/src/components/mainPage.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import MenuLeft from './menuLeft'
+import MenuLeft from './menuLeft';
+import Toolbar from './toolbar';
 
 export default class MainPage extends Component{
     render(){
         return(
             <div className="main__page">
+                <Toolbar />
                 <MenuLeft />
             </div>
         )
