644abaf0c3ae8eca7742502ac87bdd76db0b0d84
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-02-08 22:49:47 +0500

    Update toolbar.jsx

--- films/src/components/toolbar.jsx
+++ films/src/components/toolbar.jsx
@@ -5,11 +5,10 @@ export default class Toolbar extends Component{
         return(
             <div className="toolbar__main">
                 <div>
-                    <div id="menuLeft">меню</div>
+                    <a id="menuLeft">меню</a>
                     <a href="#kino">кино</a>
                     <a href="#serials">сериалы</a>
                     <a href="#anime">аниме</a>
-                    <a href="#we">мы в соцсетях</a>
                 </div>
             </div>
         )
