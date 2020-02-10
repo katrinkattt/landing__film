0b60dd41cff1df1c6e825092fe37436c869e6990
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-02-10 17:38:32 +0500

    Update post.jsx

--- films/src/components/post.jsx
+++ films/src/components/post.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default function Post(props){
     return(
         <div className="post__main">
-            <a href={props.link}></a>
+            <a className="post__link" href={props.link}></a>
             <div className="post__foto" style={{background:`url(${props.foto})`}}></div>
             <div className="post__title">{props.title}</div>
             <div className="post__text">{props.text}</div>
